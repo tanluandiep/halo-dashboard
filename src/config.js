15ca03ef06a297bfa551c6a2c26fe233fd8e2464
Diệp Tấn Luân <tanluandiep.tbec@gmail.com>
2026-05-09 01:17:07 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@
  * Halo Dashboard Configuration
  */
 export const CONFIG = {
-  // REPLACE THIS with your Google Apps Script Web App URL after deployment
-  // Example: https://script.google.com/macros/s/AKfycbz.../exec
-  API_URL: 'https://script.google.com/macros/s/AKfycbyOWxloko_UTAK2vJIQ_DHwfrsFWxmvSU2vonfmsNo79_ppnxiCxxNvRjwvv5XUIhqj/exec',
-  
-  APP_NAME: 'HALO SALES DASHBOARD',
-  VERSION: '1.0.0',
-  DEFAULT_MODE: 'month'
+    // REPLACE THIS with your Google Apps Script Web App URL after deployment
+    // Example: https://script.google.com/macros/s/AKfycbz.../exec
+    API_URL: 'https://script.google.com/macros/s/AKfycby-JMHpKb0DD4TjILRtOJIO_pFy8crkwTW8CZvdZTnjwznx9qyHQ16OvLf1sT971rnN/exec',
+
+    APP_NAME: 'HALO SALES DASHBOARD',
+    VERSION: '1.0.0',
+    DEFAULT_MODE: 'month'
 };
